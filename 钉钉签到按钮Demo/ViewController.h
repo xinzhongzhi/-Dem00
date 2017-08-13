1d758f478b7c8d6df6743dd18c20cdcc54750b07
risenb_xinzhongzhi <xinzz_sy_risenb@163.com>
2017-08-13 20:01:19 +0800

    辛忠志

--- 钉钉签到按钮Demo/ViewController.h
+++ 钉钉签到按钮Demo/ViewController.h
@@ -1,9 +1,9 @@
 //
 //  ViewController.h
-//  钉钉签到按钮Demo
+//  LLRadarView
 //
-//  Created by 辛忠志 on 2017/8/13.
-//  Copyright © 2017年 辛忠志. All rights reserved.
+//  Created by 雷亮 on 16/8/31.
+//  Copyright © 2016年 Leiliang. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- 钉钉签到按钮Demo/ViewController.m
+++ 钉钉签到按钮Demo/ViewController.m
@@ -1,14 +1,23 @@
 //
 //  ViewController.m
-//  钉钉签到按钮Demo
+//  LLRadarView
 //
-//  Created by 辛忠志 on 2017/8/13.
-//  Copyright © 2017年 辛忠志. All rights reserved.
+//  Created by 雷亮 on 16/8/31.
+//  Copyright © 2016年 Leiliang. All rights reserved.
 //
 
 #import "ViewController.h"
+#import "LLRadarView.h"
+
+
+#define HEXCOLOR(hexValue)              [UIColor colorWithRed : ((CGFloat)((hexValue & 0xFF0000) >> 16)) / 255.0 green : ((CGFloat)((hexValue & 0xFF00) >> 8)) / 255.0 blue : ((CGFloat)(hexValue & 0xFF)) / 255.0 alpha : 1.0]
+
+#define kDefaultColorValue 0x36ae29
 
 @interface ViewController ()
+
+@property (nonatomic, strong) LLRadarView *radarView;
+@property (weak, nonatomic) IBOutlet UIButton *button;
 
 @end
 
@@ -17,13 +26,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.radarView = [[LLRadarView alloc] initWithFrame:CGRectMake(50, 100, 200, 200)];
+    _radarView.showCrossline = YES;
+    _radarView.needOpacityAnimation = YES;
+    [self.view addSubview:_radarView];
+    
+    self.button.layer.cornerRadius = 40.f;
+    self.button.backgroundColor = HEXCOLOR(kDefaultColorValue);
 }
+
+- (IBAction)startAnimation:(id)sender {
+    [_radarView startAnimation];
+}
+
+- (IBAction)stopAnimation:(id)sender {
+    [_radarView stopAnimation];
+}
+
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 @end
